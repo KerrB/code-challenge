37dcb2a49d06f2c789423421bf7c215b9f806bcd
Kerr <kerr.salabao@gmail.com>
2024-07-23 22:26:30 -0500

    in progress tests and request

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,28 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
+   document.addEventListener('DOMContentLoaded', () => {
+      document.getElementById('submit').addEventListener('click', async (e) => {
+          const inputString = document.getElementById('address').value;
+          const url = `api/parse/?input_string=${encodeURIComponent(inputString)}`;
+          
+          try {
+              const response = await fetch(url, {
+                  method: "GET",
+                  headers: {
+                      'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value,
+                  }
+              });
+   
+              if (!response.ok) {
+                  throw new Error(`HTTP error! Status: ${response.status}`);
+              }
+   
+              const data = await response.json();
+              console.log(data);
+          } catch (error) {
+              console.error('Error:', error);
+          }
+      }, false);
+   });
+   
+   
